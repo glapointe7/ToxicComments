5d23a035ee90cdfd7c535c4bcd315b160235d6f4
lapoig <dragongl@gmail.com>
2018-02-09 20:51:21 -0500

    Finished Conclusion and comparison with the Perspective's current models. Removed warnings and added new functions for the comparison.

--- DataExploratory.py
+++ DataExploratory.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas
 import re
 import string
@@ -23,15 +24,29 @@ def clean_text(comments):
         ## Remove carriage returns.
         comment = re.sub("\\n", " ", comment)
         ## Remove leaky elements like IP addresses.
-        comment = re.sub("\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}", "", comment)
+        #comment = re.sub("\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}", "", comment)
         ## Remove usernames.
         comment = re.sub("\[\[.*\]", "", comment)
-        comment = re.sub("[\$\*&%#]", "", comment)
+        comment = re.sub("[\$\*&%#@\"]", " ", comment)
         ## Remove punctuation.
         comment = re.sub('\W', ' ', comment)
         ## Remove non ascii characters.
         comment = ''.join([char for char in ascii_chars_from_text(comment, ascii_chars)])
+        comment = re.sub("fck", "fuck", comment)
+        comment = re.sub("f ck", "fuck", comment)
+        comment = re.sub("fagget", "faggot", comment)
+        comment = re.sub("you re", "you are", comment)
+        comment = re.sub("\d", " ", comment)
 
         train_comments_cleaned.append(comment)
         
     return pandas.Series(train_comments_cleaned).astype(str)
+
+def clean(train_perspective):
+    classes = ['comment', 'is_toxic']
+    train = train_perspective.loc[:, classes]
+
+    train.comment = clean_text(train.comment)
+    train.is_toxic = train.is_toxic.astype(np.int64)
+    
+    return train
--- Model.py
+++ Model.py
@@ -1,17 +1,22 @@
 import numpy as np
+
 from sklearn.metrics import roc_auc_score
+from sklearn.svm import LinearSVC
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
 
 
-def calculate_predictions(classifier, X_train_vect, X_test_vect, y_train, y_test, output_classes):
+def calculate_predictions(classifier, X_train, X_test, y_train, y_test, output_classes):
     roc_auc = []
     predictions = np.zeros(shape=(len(y_test), len(output_classes)))
     predictions_int = np.zeros(shape=(len(y_test), len(output_classes)))
     
     for i, output_class in enumerate(output_classes):
-        classifier.fit(X_train_vect, y_train[output_class])
+        classifier.fit(X_train, y_train[output_class])
         
-        predictions[:, i] = classifier.predict_proba(X_test_vect)[:, 1]
-        predictions_int[:, i] = classifier.predict(X_test_vect)
+        predictions[:, i] = classifier.predict_proba(X_test)[:, 1]
+        predictions_int[:, i] = classifier.predict(X_test)
         auc = roc_auc_score(y_test[output_class], predictions[:, i])
         roc_auc.append(auc)
         print("\nClass: ", output_class)
@@ -21,6 +26,21 @@ def calculate_predictions(classifier, X_train_vect, X_test_vect, y_train, y_test
     
     return roc_auc, predictions, predictions_int
 
+
+def apply_best_model(dataset_name, train, test):
+    vectorizer = TfidfVectorizer(sublinear_tf=True)
+    X_train_vect = vectorizer.fit_transform(train.comment)
+    X_test_vect = vectorizer.transform(test.comment)
+
+    svm = LinearSVC(C=0.22)
+    classifier = CalibratedClassifierCV(svm)
+
+    classifier.fit(X_train_vect, train.is_toxic)
+
+    predictions = classifier.predict_proba(X_test_vect)[:, 1]
+    auc_roc = roc_auc_score(test.is_toxic, predictions)
+    print(dataset_name + " AUC ROC: ", auc_roc)
+    
 
 def multi_class_rocauc(y_test, predictions):
     total_roc_auc = 0
